c4335fc16e6328b2bef17691f9c266d22891a1c2
Tiago Patricio <69849680+Tiago-Patricio@users.noreply.github.com>
2022-04-02 00:15:23 +0100

    Implementation of an undirected graph

---
+++ data_structures/Graph.js
@@ -0,0 +1,34 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(value) {
+    if (!this.adjacencyList[value]) {
+      this.adjacencyList[value] = [];
+    }
+  }
+
+  //undirected graph
+  addEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+      (v) => v !== vertex2
+    );
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+      (v) => v !== vertex1
+    );
+  }
+}
